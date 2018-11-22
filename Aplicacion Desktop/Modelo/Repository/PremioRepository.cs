3b68b4a5038d825cae52df807d7b72f65b10b17a
Juan Carlos Mujica <mujica.juancarlos@gmail.com>
2018-11-22 01:08:34 -0300

    Add repositories

---
+++ Aplicacion Desktop/Modelo/Repository/PremioRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Repository;
+
+namespace Modelo.Repository
+{
+    class PremioRepository : RepositoryBase<Premio>
+    {
+    }
+}
